31e5e8cc637c41ae61af0063bcc733b70dd1f5b8
Joao Oliveira <j.oliveiradg@gmail.com>
2021-07-05 10:57:14 -0300

    ProjetoCRUD_ReactJS
    ProjetoCRUD_ReactJS

---
+++ frontend/src/components/template/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+    <footer className="footer">
+        <span>
+            Desenvolvido com <i className="fa fa-heart text-danger"></i> por
+            <strong> João<span className="text-danger">Oliveira</span></strong>
+        </span>
+    </footer>
